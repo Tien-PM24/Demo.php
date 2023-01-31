0211cd5e5c26a5f4517c2a3841a5ff626dcef057
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-01-31 11:54:37 +0700

    push demo php

---
+++ demo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $mung = 7;
+    switch($mung) {
+        case 1:
+            echo "Visit Papa";
+        case 2:
+            echo "Visit Mama";
+        case 3:
+            echo "Visit Myfriends";
+        case 4:
+            echo "Sleep every Day";
+        case 5:
+            echo "Play football in game";
+        case 6:
+            echo "Work some thing to do";
+        case 7:
+            echo "Return PNV";
+    }
+    ?>
+</body>
+</html>
