4d76bfdf9da5be1fc57ccbafddc960e03cf9fc01
Tien-PM24 <115676176+Tien-PM24@users.noreply.github.com>
2023-02-06 01:05:33 +0700

    Update and fix error

--- demo.php
+++ demo.php
@@ -6,25 +6,97 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    body{
+        background: #008b8b;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    form,button,.check{
+        text-align: center;
+    }
+    b{
+        font-size: 35px;
+        color: #ff7f50;
+    }
+    span{
+        font-size: 35px;
+        color: aqua; 
+    }
+    div{
+        font-family: timenewsroman;
+        font-size: 20px;
+    }
+    div,.text1{
+        color: #ffebcd;
+    }
+    div,.text2{
+        color: #00FFFF;
+    }
+    div,#mung{
+        height: 25px;
+    }
+    div,#day{
+        height: 25px;
+    }
+    button{
+        font-size:15px;
+        font-weight:bold;
+        height: 30px;
+        width: 90px;
+        background: #FFCCCC;
+        border: 3px solid purple;
+        border-radius: 10px;
+        margin-top: 10px;
+        justify-content: center;
+    }
+    button:hover{
+        cursor: pointer;
+    }
+    button:hover{
+        background-color: orange;
+        font-size: 100%;
+        color: blue;
+    }
+</style>
 <body>
     <?php
-    $mung = 4;
-    switch($mung) {
+    $day = $_POST["mung"];
+    switch($day) {
         case 1:
-            echo "Visit Papa"; break;
+            $day = "Visit Papa"; break; 
         case 2:
-            echo "Visit Mama"; break;
+            $day = "Visit Mama"; break;
         case 3:
-            echo "Visit Myfriends"; break;
+            $day = "Visit Myfriends"; break;
         case 4:
-            echo "Sleep every Day"; break;
+            $day = "Sleep every Day"; break;
         case 5:
-            echo "Play football in game"; break;
+            $day = "Play football in game"; break;
         case 6:
-            echo "Work some thing to do"; break;
+            $day = "Work some thing to do"; break;
         case 7:
-            echo "Return PNV"; break;
+            $day = "Return PNV"; break;
+        case 8:
+            $day = "This day finish Tet holiday"; break;
     }
     ?>
+
+    <form action="demo.php" method = "post">
+    <b>WHAT SHOULD <span>YOU DO ??</span></b><br><br>
+        <div class="text1">
+            Enter today's date:
+            <input type="text" name="mung" id="mung" placeholder="Click here" style="background-color: #006666">
+        </div><br>
+        <div class="text2">
+            Today you going to: 
+            <input type="text" name="day" id="day" value="<?php echo $day;?>" readonly="true" style="background-color: #99CCFF">
+        </div><br>
+        <button class="check" type="submit" name="submit">Check</button>
+        
+    </form>
 </body>
 </html>
